5b15d5bf1813e88cf9af12f52e578fbcec00b8c6
foxie999 <samrazor2010@gmail.com>
2022-11-28 11:59:33 -0600

    initial commit

---
+++ big_patch_mod/Config.cpp
@@ -0,0 +1,610 @@
+class CfgPatches {
+
+	class big_patch_mod
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0.0";
+		author[]= {"Various Artists"};
+		authorUrl = "";
+	};
+
+};
+
+class CfgUnitInsignia
+{
+	
+	class wgw_1st_japari
+	{
+		displayName = "WGW 1st Japari Marines";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_1st_japari.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_xenomorph
+	{
+		displayName = "WGW Xenomorphs";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_xenomorph.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_anti_poaching
+	{
+		displayName = "WGW Anti Poaching Unit";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_anti_poaching.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_best_job
+	{
+		displayName = "WGW Best Job I Eever Had";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_best_job.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_celadon_police
+	{
+		displayName = "WGW Celadon City Police";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_celadon_police.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_dive_team
+	{
+		displayName = "WGW Cinnabar Island Dive Team";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_dive_team.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_embrace_the_hate
+	{
+		displayName = "WGW Embrace the Hate";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_embrace_the_hate.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_even_death
+	{
+		displayName = "WGW Even Death Can't Stop Me";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_even_death.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_forest_ranger
+	{
+		displayName = "WGW Viridian Forest Ranger";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_forest_ranger.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_fox_friend
+	{
+		displayName = "WGW Fox Friends Special Forces";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_fox_friend.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_gatekeeper
+	{
+		displayName = "WGW Cerulean Gatekeepers";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_gatekeeper.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_gfl_springfield
+	{
+		displayName = "WGW 30-06 Springfield";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_gfl_springfield.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_give_nothing
+	{
+		displayName = "WGW Give Nothing Take Everything";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_give_nothing.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_holo
+	{
+		displayName = "WGW Holo";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_holo.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_japari_sugoi_friends
+	{
+		displayName = "WGW Japai Sugoi Friends";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_japari_sugoi_friends.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_kanto_regional_fire
+	{
+		displayName = "WGW Kanto Regional Fire";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_kanto_regional_fire.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_kanto_regional_police
+	{
+		displayName = "WGW Kanto Regional Police";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_kanto_regional_police.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_nocturnal_friends
+	{
+		displayName = "WGW Nocturnal Friends";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_nocturnal_friends.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_overkill
+	{
+		displayName = "WGW No Kill Like Overkill";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_overkill.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_paranormal_investigator
+	{
+		displayName = "WGW Paranormal Investigators";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_paranormal_investigator.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_port_authority
+	{
+		displayName = "WGW Vermilion Port Authority";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_port_authority.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_search_and_rescue
+	{
+		displayName = "WGW Mt Moon Search and Rescue";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_search_and_rescue.paa";
+		textureVehicle = "";
+	};
+	
+	class wgw_silph_co_special_projects
+	{
+		displayName = "WGW Silph Co. Special Projects";
+		author = "";
+		texture = "\big_patch_mod\icon\wgw_silph_co_special_projects.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_1_armor_1
+	{
+		displayName = "WW2 British 1st Armoured Div 1";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_1_armor_1.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_1_armor_2
+	{
+		displayName = "WW2 British 1st Armoured Div 2";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_1_armor_2.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_3_inf
+	{
+		displayName = "WW2 British 3rd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_3_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_6_armor
+	{
+		displayName = "WW2 British 6th Armoured Div";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_6_armor.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_23_inf
+	{
+		displayName = "WW2 British 23rd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_23_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_48_inf
+	{
+		displayName = "WW2 british 48th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_48_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_52_mtn
+	{
+		displayName = "WW2 British 52nd Mountain Brigade";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_52_mtn.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_brits_56_inf
+	{
+		displayName = "WW2 British 56th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_brits_56_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_can_6_inf
+	{
+		displayName = "WW2 Canadian 6th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_can_6_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_1_inf
+	{
+		displayName = "WW2 US 1st Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_1_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_6_abn
+	{
+		displayName = "WW2 US 6th Airborne";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_6_abn.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_17_abn
+	{
+		displayName = "WW2 US 17th Airborne";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_17_abn.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_21_abn
+	{
+		displayName = "WW2 US 21st Airborne";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_21_abn.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_22_inf
+	{
+		displayName = "WW2 US 22nd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_22_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_25_inf
+	{
+		displayName = "WW2 US 25th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_25_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_42_inf
+	{
+		displayName = "WW2 US 42nd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_42_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_46_inf
+	{
+		displayName = "WW2 US 46th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_46_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_66_inf
+	{
+		displayName = "WW2 US 66th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_66_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_70_inf
+	{
+		displayName = "WW2 US 70th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_70_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_84_inf
+	{
+		displayName = "WW2 US 84th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_84_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_91_inf
+	{
+		displayName = "WW2 US 91st Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_91_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_92_inf
+	{
+		displayName = "WW2 US 92nd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_92_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_100_inf
+	{
+		displayName = "WW2 US 100th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_100_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_101_abn
+	{
+		displayName = "WW2 US 101st Airborne";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_101_abn.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_103_inf
+	{
+		displayName = "WW2 US 103rd Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_103_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_104_inf
+	{
+		displayName = "WW2 US 104th Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_104_inf.paa";
+		textureVehicle = "";
+	};
+	
+	class ww2_us_armor
+	{
+		displayName = "WW2 US Armoured Div";
+		author = "";
+		texture = "\big_patch_mod\icon\ww2_us_armor.paa";
+		textureVehicle = "";
+	};
+	
+	class can_air_ops
+	{
+		displayName = "Canadian Air Ops";
+		author = "";
+		texture = "\big_patch_mod\icon\can_air_ops.paa";
+		textureVehicle = "";
+	};
+	
+	class can_armor
+	{
+		displayName = "Canadian Armor";
+		author = "";
+		texture = "\big_patch_mod\icon\can_armor.paa";
+		textureVehicle = "";
+	};
+	
+	class can_army
+	{
+		displayName = "Canadian Army";
+		author = "";
+		texture = "\big_patch_mod\icon\can_army.paa";
+		textureVehicle = "";
+	};
+	
+	class can_artilltery
+	{
+		displayName = "Canadian Artillery";
+		author = "";
+		texture = "\big_patch_mod\icon\can_artilltery.paa";
+		textureVehicle = "";
+	};
+	
+	class can_electronics
+	{
+		displayName = "Canadian Electronics Div";
+		author = "";
+		texture = "\big_patch_mod\icon\can_electronics.paa";
+		textureVehicle = "";
+	};
+	
+	class can_engineer
+	{
+		displayName = "Canadian Engineers Branch";
+		author = "";
+		texture = "\big_patch_mod\icon\can_engineer.paa";
+		textureVehicle = "";
+	};
+	
+	class can_infantry
+	{
+		displayName = "Canadian Infantry";
+		author = "";
+		texture = "\big_patch_mod\icon\can_infantry.paa";
+		textureVehicle = "";
+	};
+	
+	class can_intelligence
+	{
+		displayName = "Canadian Intelligence Branch";
+		author = "";
+		texture = "\big_patch_mod\icon\can_intelligence.paa";
+		textureVehicle = "";
+	};
+	
+	class can_medic
+	{
+		displayName = "Canadian Medics Branch";
+		author = "";
+		texture = "\big_patch_mod\icon\can_medic.paa";
+		textureVehicle = "";
+	};
+	
+	class can_navy
+	{
+		displayName = "Canadian Navy";
+		author = "";
+		texture = "\big_patch_mod\icon\can_navy.paa";
+		textureVehicle = "";
+	};
+	
+	class can_sf
+	{
+		displayName = "Canadian Special Forces";
+		author = "";
+		texture = "\big_patch_mod\icon\can_sf.paa";
+		textureVehicle = "";
+	};
+	
+	class us_army_cyber_cmd
+	{
+		displayName = "US Army Cyber Command";
+		author = "";
+		texture = "\big_patch_mod\icon\us_army_cyber_cmd.paa";
+		textureVehicle = "";
+	};
+	
+	class us_army_jpn
+	{
+		displayName = "US Army of Japan";
+		author = "";
+		texture = "\big_patch_mod\icon\us_army_jpn.paa";
+		textureVehicle = "";
+	};
+	
+	class us_information_warfare
+	{
+		displayName = "US Information Warfare";
+		author = "";
+		texture = "\big_patch_mod\icon\us_information_warfare.paa";
+		textureVehicle = "";
+	};
+	
+	class v_f4
+	{
+		displayName = "Vehicle F4 Phantom II";
+		author = "";
+		texture = "\big_patch_mod\icon\v_f4.paa";
+		textureVehicle = "";
+	};
+	
+	class v_uh1
+	{
+		displayName = "Vehicle UH-1 Huey";
+		author = "";
+		texture = "\big_patch_mod\icon\v_uh1.paa";
+		textureVehicle = "";
+	};
+	
+	class wep_amraam
+	{
+		displayName = "Weapon AMRAAM";
+		author = "";
+		texture = "\big_patch_mod\icon\wep_amraam.paa";
+		textureVehicle = "";
+	};
+	
+	class wep_sidewinder
+	{
+		displayName = "Weapon Sidewinder";
+		author = "";
+		texture = "\big_patch_mod\icon\wep_sidewinder.paa";
+		textureVehicle = "";
+	};
+	
+	class wep_sparrow
+	{
+		displayName = "Weapon Sparrow";
+		author = "";
+		texture = "\big_patch_mod\icon\wep_sparrow.paa";
+		textureVehicle = "";
+	};
+	
+	class 12accs_1
+	{
+		displayName = "12th Airborne Command Control Squadron 1";
+		author = "";
+		texture = "\big_patch_mod\icon\12accs_1.paa";
+		textureVehicle = "";
+	};
+	
+	class 12accs_2
+	{
+		displayName = "12th Airborne Command Control Squadron 2";
+		author = "";
+		texture = "\big_patch_mod\icon\12accs_2.paa";
+		textureVehicle = "";
+	};
+	
+	class 12accs_3
+	{
+		displayName = "12th Airborne Command Control Squadron 3";
+		author = "";
+		texture = "\big_patch_mod\icon\12accs_3.paa";
+		textureVehicle = "";
+	};
+	
+	class ohgodohduck
+	{
+		displayName = "Oh God Oh Duck";
+		author = "";
+		texture = "\big_patch_mod\icon\ohgodohduck.paa";
+		textureVehicle = "";
+	};
+	
+	class squirtlecool
+	{
+		displayName = "Squirtle Cool";
+		author = "";
+		texture = "\big_patch_mod\icon\squirtlecool.paa";
+		textureVehicle = "";
+	};
+	
+};
