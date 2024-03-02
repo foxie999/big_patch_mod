3b4e78ce97c41688314576ae590b86fc270c8ceb
foxie999 <110860407+foxie999@users.noreply.github.com>
2024-03-01 19:16:53 -0800

    foxy attempts to fix her mod
    redoing entire file naming system among other things

--- big_patch_mod/Config.cpp
+++ big_patch_mod/Config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredAddons[] = {};
-		version = "1.0.0";
+		version = "1.2.0";
 		author[] = {"Various Artists"};
 		authorUrl = "";
 	};
@@ -15,91 +15,131 @@ class CfgPatches {
 class CfgUnitInsignia
 {
 	
-	class can_air_ops
+	class 12accs1
 	{
-		displayName = "Canadian Air Ops";
+		displayName = "00 12th ACCS 1";
 		author = "";
-		texture = "\big_patch_mod\icon\can_air_ops.paa";
+		texture = "\big_patch_mod\icon\12accs1.paa";
 		textureVehicle = "";
 	};
 	
-	class can_armor
+	class 12accs2
 	{
-		displayName = "Canadian Armor";
+		displayName = "00 12th ACCS 2";
 		author = "";
-		texture = "\big_patch_mod\icon\can_armor.paa";
+		texture = "\big_patch_mod\icon\12accs2.paa";
 		textureVehicle = "";
 	};
 	
-	class can_army
+	class 12accs3
 	{
-		displayName = "Canadian Army";
+		displayName = "00 12th ACCS 3";
 		author = "";
-		texture = "\big_patch_mod\icon\can_army.paa";
+		texture = "\big_patch_mod\icon\12accs3.paa";
 		textureVehicle = "";
 	};
 	
-	class can_artilltery
+	class canada1
 	{
-		displayName = "Canadian Artillery";
+		displayName = "01 Royal Canadian AF 1";
 		author = "";
-		texture = "\big_patch_mod\icon\can_artilltery.paa";
+		texture = "\big_patch_mod\icon\canada1.paa";
 		textureVehicle = "";
 	};
 	
-	class can_electronics
+	class canada2
 	{
-		displayName = "Canadian Electronics Div";
+		displayName = "01 Canadian Armored Division";
 		author = "";
-		texture = "\big_patch_mod\icon\can_electronics.paa";
+		texture = "\big_patch_mod\icon\canada2.paa";
 		textureVehicle = "";
 	};
 	
-	class can_engineer
+	class canada3
 	{
-		displayName = "Canadian Engineers Branch";
+		displayName = "01 Canadian Army";
 		author = "";
-		texture = "\big_patch_mod\icon\can_engineer.paa";
+		texture = "\big_patch_mod\icon\canada3.paa";
 		textureVehicle = "";
 	};
 	
-	class can_infantry
+	class canada4
 	{
-		displayName = "Canadian Infantry";
+		displayName = "01 Canadian Artillery Regiment";
 		author = "";
-		texture = "\big_patch_mod\icon\can_infantry.paa";
+		texture = "\big_patch_mod\icon\canada4.paa";
 		textureVehicle = "";
 	};
 	
-	class can_intelligence
+	class canada5
 	{
-		displayName = "Canadian Intelligence Branch";
+		displayName = "01 Canadian Cyber-Warfare";
 		author = "";
-		texture = "\big_patch_mod\icon\can_intelligence.paa";
+		texture = "\big_patch_mod\icon\canada5.paa";
 		textureVehicle = "";
 	};
 	
-	class can_medic
+	class canada6
 	{
-		displayName = "Canadian Medics Branch";
+		displayName = "01 Canadian Engineers Regiment";
 		author = "";
-		texture = "\big_patch_mod\icon\can_medic.paa";
+		texture = "\big_patch_mod\icon\canada6.paa";
 		textureVehicle = "";
 	};
 	
-	class can_navy
+	class canada7
 	{
-		displayName = "Canadian Navy";
+		displayName = "01 Canadian Infantry Regiment";
 		author = "";
-		texture = "\big_patch_mod\icon\can_navy.paa";
+		texture = "\big_patch_mod\icon\canada7.paa";
 		textureVehicle = "";
 	};
 	
-	class can_sf
+	class canada8
 	{
-		displayName = "Canadian Special Forces";
+		displayName = "01 Canada Intel Command";
 		author = "";
-		texture = "\big_patch_mod\icon\can_sf.paa";
+		texture = "\big_patch_mod\icon\canada8.paa";
+		textureVehicle = "";
+	};
+	
+	class canada9
+	{
+		displayName = "01 Canada JOC";
+		author = "";
+		texture = "\big_patch_mod\icon\canada9.paa";
+		textureVehicle = "";
+	};
+	
+	class canada10
+	{
+		displayName = "01 Canadian Medics Regiment";
+		author = "";
+		texture = "\big_patch_mod\icon\canada10.paa";
+		textureVehicle = "";
+	};
+	
+	class canada11
+	{
+		displayName = "01 Royal Canadian Navy";
+		author = "";
+		texture = "\big_patch_mod\icon\canada11.paa";
+		textureVehicle = "";
+	};
+	
+	class canada12
+	{
+		displayName = "01 Royal Canadian AF 2";
+		author = "";
+		texture = "\big_patch_mod\icon\canada12.paa";
+		textureVehicle = "";
+	};
+	
+	class canada13
+	{
+		displayName = "01 Canadian Special Forces";
+		author = "";
+		texture = "\big_patch_mod\icon\canada13.paa";
 		textureVehicle = "";
 	};
 };
